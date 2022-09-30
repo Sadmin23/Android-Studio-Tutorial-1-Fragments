7c74f4bd6f0f7637b8046726b051d36c43575990
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2022-09-30 11:06:11 +0600

    fragment2 and its xml file

---
+++ app/src/main/java/com/example/fragmenttutorial/fragment2.java
@@ -0,0 +1,30 @@
+package com.example.fragmenttutorial;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class fragment2 extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_fragment2, container, false);
+
+        return view;
+    }
+}
